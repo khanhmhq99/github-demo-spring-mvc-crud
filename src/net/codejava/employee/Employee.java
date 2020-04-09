910a71400689876ab0e5fe5ac0d42be3faf2ac68
truong nguyen <huutruong15599@gmail.com>
2020-04-09 15:32:41 +0700

    added persistence, sql and jpa config

--- src/net/codejava/employee/Employee.java
+++ src/net/codejava/employee/Employee.java
@@ -1,46 +1,44 @@
 package net.codejava.employee;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	private int id;
+@Entity
+
+public class Employee {
+	private Integer EmployeeID;
 	private String name;
 	private String email;
 	private String address;
 	
-	public Employee() {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getEmployeeID() {
+		return EmployeeID;
 	}
-
-	public int getId() {
-		return id;
+	public void setEmployeeID(Integer employeeID) {
+		EmployeeID = employeeID;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
-	
 }
--- src/net/codejava/employee/config/JpaConfig.java
+++ src/net/codejava/employee/config/JpaConfig.java
@@ -1,10 +1,31 @@
 package net.codejava.employee.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"net.codejava.employee"})
 public class JpaConfig {
+	static EntityManagerFactory factory;
+	static EntityManager createEntityManager;
+	
+	private static void begin() {
+
+		factory = Persistence.createEntityManagerFactory("EmployeeUnit");
+		
+		createEntityManager = factory.createEntityManager();
+
+		createEntityManager.getTransaction().begin();
+	}
+	
+	private static void end() {
+		createEntityManager.getTransaction().commit();
+		createEntityManager.close();
+		factory.close();
+	}
 
 }
